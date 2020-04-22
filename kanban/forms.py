597bc6125e76ee4fc1eb0d65bfb943aa50930e71
kazuya <kazuyza.osaka.2@gmail.com>
2020-04-22 22:32:34 +0900

    initialize repository

--- kanban/forms.py
+++ kanban/forms.py
@@ -3,7 +3,13 @@ from django.contrib.auth.models import User
 
 from .models import List, Card
 
+
 class UserForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = User
@@ -12,12 +18,22 @@ class UserForm(forms.ModelForm):
 
 class ListForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(ListForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
-        model =  List
+        model = List
         fields = ("title",)
 
 
 class CardForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = Card
@@ -25,7 +41,24 @@ class CardForm(forms.ModelForm):
 
 
 class CardCreateFromHomeForm(forms.ModelForm):
-    
+
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Card
         fields = ("title", "description",)
+
+
+class CardUpdateForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardUpdateForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Card
+        fields = ("title", "description", "list")
